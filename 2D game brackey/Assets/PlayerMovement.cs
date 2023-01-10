3f1a4dd14a7f2d5146601a558ddecb7fe78fc7e1
STRIX\Phil <philmottin@gmail.com>
2023-01-10 07:18:05 +0200

    character controller - jump animation fix

--- 2D game brackey/Assets/PlayerMovement.cs
+++ 2D game brackey/Assets/PlayerMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    public float horizontalMove = 0;
+    
+    float horizontalMove = 0;
     public float runSpeed = 40f;
     bool jump = false;
     bool crouch = false;
-
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,14 +20,33 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        /*
+         * The jump animation only works with the 'Has exit time' checked on both transitions
+         * OR changed CharacterController2D.cs on line 130 
+         * // Add a vertical force to the player. from false to true
+		 *	m_Grounded = false;
+		 *
+		 * // best solution, change this radius
+         * OR const float k_GroundedRadius = .2f; changed to 0.05 - line 
+         */
         if (Input.GetButtonDown("Jump")) {
             jump = true;
+            animator.SetBool("isJumping", true);
         }
         if (Input.GetButtonDown("Crouch")) {
             crouch = true;
         } else if (Input.GetButtonUp("Crouch")) {
             crouch = false;
         }
+    }
+
+    public void onLanding() {
+        animator.SetBool("isJumping", false);
+    }
+    public void onCrouching(bool isCrouching) {
+        animator.SetBool("isCrouching", isCrouching);
     }
 
     void FixedUpdate() {
